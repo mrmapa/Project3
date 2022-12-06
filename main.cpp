d6f8fe3bb8444e874e76f5a39543cd2c748a94fb
Derek Mo <derek.mo7203@gmail.com>
2022-12-06 00:28:01 -0500

    anime list parsing, url fix, and graph class

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <SFML/Graphics.hpp>
 #include "TextField.h"
 #include <iostream>
@@ -35,3 +36,70 @@ int main() {
     return 0;
 }
 // Mapa was here
+*/
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include "Graph.h"
+using namespace std;
+
+void loadCSV(const string& filename, map<int, Anime>& mapper) {
+    ifstream inFile(filename);
+
+    //reads the first line of data
+    string lineFromFile;
+    getline(inFile, lineFromFile);
+
+    int index = 0;
+
+    while(getline(inFile, lineFromFile) && index < 1000){
+        //stream of data from a string
+        istringstream stream(lineFromFile);
+
+        int animeID;
+        string title;
+        string imageURL;
+        const string urlFix = "https://cdn.myanimelist.net";
+        string genre;
+        string temp;
+
+        getline(stream, temp, ';');
+        animeID = stoi(temp);
+        string name;
+        getline(stream, name, ';');
+        getline(stream, temp, ';');
+        getline(stream, temp, ';');
+        getline(stream, temp, ';');
+        getline(stream, imageURL, ';');
+
+        for(int i = 0; i < 22; i++) {
+            getline(stream, temp, ';');
+        }
+
+        getline(stream, genre, ';');
+
+        imageURL.replace(0, 32, urlFix);
+
+        //creates an object from the information
+        Anime anime(animeID, name, imageURL, genre);
+        mapper[index] = anime;
+        //mapper[index].Print();
+        index++;
+    }
+}
+
+int main() {
+    map<int, Anime> mapper;
+
+    loadCSV("C:/Users/derek/Documents/GitHub/Project3/src/anime_filtered_modified.csv", mapper);
+
+    for(auto iter: mapper) {
+        iter.second.Print();
+    }
+
+
+    return 0;
+}
